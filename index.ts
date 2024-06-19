8d1f0f6fc5274df723f141a595749fd8dcab88f5
Bryan Arroyave Ortiz <barroyave@leal.co>
2024-06-19 18:15:41 -0500

    remove src folder

---
+++ index.ts
@@ -0,0 +1,12 @@
+// ATOMS
+
+export { default as Button } from './components/atoms/Button'
+// import Heading from './components/atoms/Heading'
+// import Icon from './components/atoms/Icon'
+// import Picture from './components/atoms/Picture'
+
+// MOLECULES
+
+// import ButtonIcon from './components/molecules/ButtonIcon'
+
+// export { Button, Heading, Icon, Picture, ButtonIcon }
